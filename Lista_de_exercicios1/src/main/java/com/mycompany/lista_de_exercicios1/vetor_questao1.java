0c397b02b4a22fff67534f0171ae2a6c9e15c6b6
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-02-04 15:36:42 -0300

    Terminei os exercicios sobre vetor

--- Lista_de_exercicios1/src/main/java/com/mycompany/lista_de_exercicios1/vetor_questao1.java
+++ Lista_de_exercicios1/src/main/java/com/mycompany/lista_de_exercicios1/vetor_questao1.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.lista_de_exercicios1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kiran
@@ -15,7 +17,54 @@ public class vetor_questao1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*1. Elabore um programa que preencha um vetor com os valores 8 valores 
+        digitados pelo usuário. Imprima o vetor. Em seguida imprima o vetor ao contrário.*/
+        /*int numero[]=new int[8];
+        Scanner ler= new Scanner(System.in);
+        for(int i=0;i<numero.length;i++){
+            System.out.println("Escreva um número:");
+            numero[i]=ler.nextInt();
+        }
+        for(int i=0;i<numero.length;i++){
+            System.out.println(numero[i]);
+        }
+        System.out.println("-------------------------------------------------");
+        for(int i=numero.length-1;i>=0;i--){
+            System.out.println(numero[i]);
+        }*/
+        
+        /*2. Leia 6 números e armazene-os em um vetor. 
+        Em seguida percorrer o vetor e mostrar se tem o 10 número no vetor e em qual posição.*/
+        /*int numeros[] = new int[6];
+        Scanner ler=new Scanner(System.in);
+        for(int i=0;i<numeros.length;i++){
+            System.out.println("Escreva um numero");
+            numeros[i]=ler.nextInt();
+        }
+        System.out.println("O número 10 foi encontrado em:\n-------------------------------");
+        System.out.println("Indixe    Valor\n");
+        for(int i=0;i<numeros.length;i++){
+            if(numeros[i]==10){
+                System.out.println("   "+i+"        "+numeros[i]);
+            }
+        }*/
+        
+        /*3. Criar um programa para ler 10 números inteiros em um vetor A, 
+        criar um vetor B que  deverá  receber  os  valores  multiplicados  por  3.  
+        Apresentar  os  valores  dos  2 vetores.*/
+        int A[] = new int[10];
+        int B[] = new int[10];
+        Scanner ler=new Scanner(System.in);
+        for(int i=0;i<A.length;i++){
+            System.out.println("Escreva um número:");
+            A[i]=ler.nextInt();
+            B[i]=A[i]*3;
+        }
+        System.out.println("Vetor A   Vetor B\n");
+        for(int i=0;i<B.length;i++){
+           System.out.println("   "+A[i]+"         "+B[i]);
+        }
+        
     }
     
 }

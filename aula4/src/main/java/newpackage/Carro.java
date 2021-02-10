dd8a384c8af6cf9b8370c5f9bfe5168f1134d500
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-02-10 11:09:19 -0300

    exercicio FEITO

---
+++ aula4/src/main/java/newpackage/Carro.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author kiran
+ */
+public class Carro {
+    String modelo;
+    int ano;
+    String marca;
+    String cor;
+    String chacci;
+    
+    public Carro(String modelo, int ano, String marca,
+    String cor, String chacci){
+        this.modelo=modelo;
+        this.ano=ano;
+        this.marca=marca;
+        this.cor=cor;
+        this.chacci=chacci;
+    }//fazer carro e retangulo
+    
+    public void mostrar(){
+        System.out.println("Modelo: "+modelo);
+        System.out.println("ano: "+ano);
+        System.out.println("marca: "+marca);
+        System.out.println("cor: "+cor);
+        System.out.println("chacci: "+chacci);
+    }
+    
+    public void ligar(){
+        System.out.println("Carro ligado");
+    }
+    public void acelerar(){
+        System.out.println("Asselerando o carro");
+    }
+    public void freia(){
+        System.out.println("Freiando o carro");
+    }
+    public void desligar(){
+        System.out.println("Carro desligado");
+    }
+}
---
+++ aula4/src/main/java/newpackage/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kiran
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Escreva o Modelo, o ano, a Marca e o chacci de um carro");
+        Carro carro1=new Carro("fusca", 1999, "forty", "azul", "0000-00000");
+        Carro carro2=new Carro("picape", 2005, "forty", "preto", "1111-1111");
+        carro1.mostrar();
+        carro1.ligar();
+        carro1.acelerar();
+        carro2.mostrar();
+        carro2.freia();
+        carro2.desligar();
+    }
+    
+}
